63d4743131fe7a2806006c2006d64069353272dc
mstn <mstn@git.com>
2015-11-12 17:55:54 +0100

    reactive loader for static assets

---
+++ loader_client.js
@@ -0,0 +1,86 @@
+"use strict"
+
+const handleComplete = function(event){
+  const self = this;
+  Meteor.setTimeout(function(){
+    self.status.set('complete', true);
+  }, self.options.waitAfterComplete || 0);
+};
+
+const handleError = function(event){
+  this.status.set('error', {
+    error: event.title,
+    reason: event.message,
+    details: event.data
+  });
+};
+
+const handleProgress = function(event){
+  this.status.set('progress', event.progress);
+  this.status.set('total', event.total);
+  this.status.set('loaded', event.loaded);
+};
+
+const handleFileload = function(){
+  // TODO handle individual file loading
+};
+
+const handleFileprogress = function(){
+  // TODO handle individual file loading
+};
+
+class Handle {
+  constructor(loader){
+    this.loader = loader
+  }
+  ready(){
+    return this.loader.isReady();  
+  }
+};
+
+Loader = class {
+
+  constructor(options){
+
+    this.options = options || {};
+    
+    this.uid = Random.id();
+    this.queue = new createjs.LoadQueue();
+    this.status = new ReactiveDict('_loader_status_'+this.uid);
+
+    this.queue.on('complete', handleComplete, this);
+    this.queue.on('error', handleError, this);
+    this.queue.on('progress', handleProgress, this);
+    this.queue.on('fileload', handleFileload, this);
+    this.queue.on('fileprogress', handleFileprogress, this);
+
+  }
+
+  addFiles(files){
+    const loadNow = false;
+    let fileCount = files.length;
+    if (fileCount === 1){
+      this.queue.loadFile(files[0], loadNow);
+    } else {
+      this.queue.loadManifest(files, loadNow);
+    }
+  }
+
+  status(){
+   return this.status.all(); 
+  }
+
+  isReady(){
+    return !!this.status.get('complete');
+  }
+
+  isFailed(){
+    return !!this.status.get('error');
+  }
+
+  load(){
+    this.queue.load();
+    return new Handle(this);
+  }
+};
+
---
+++ package.js
@@ -0,0 +1,19 @@
+Package.describe({
+  summary:'load static assets (based on PreloadJS)',
+  version:'0.0.1',
+  name:'mstn:loader'
+});
+
+Package.onUse(function(api){ 
+  api.versionsFrom('1.2'); 
+
+  api.use('random', 'client');
+  api.use('reactive-dict', 'client');
+
+  api.addFiles([
+    'bower_components/PreloadJS/lib/preloadjs-0.6.1.combined.js',
+    'loader_client.js'
+  ], 'client');
+
+  api.export('Loader', 'client');
+});
